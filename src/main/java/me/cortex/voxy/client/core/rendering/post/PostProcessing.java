6149ce9466f1acb36c10b1aa9e74746733286f81
Xenthio <thegooster88@gmail.com>
2024-09-20 15:39:35 +1000

    Disable offset once done

--- src/main/java/me/cortex/voxy/client/core/rendering/post/PostProcessing.java
+++ src/main/java/me/cortex/voxy/client/core/rendering/post/PostProcessing.java
@@ -197,6 +197,7 @@ public class PostProcessing {
         this.blitTexture.blit();
         glDisable(GL_DEPTH_TEST);
         glDepthMask(true);
+        glDisable( GL_POLYGON_OFFSET_FILL );
 
         this.glStateCapture.restore();
     }
